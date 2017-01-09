5c58313f712c3a671e23302afad78834c787727f
alireza-amirsamimi <alireza.amirsamimi@gmail.com>
2017-01-09 20:58:43 +0330

    This file is added before by mistake! 	deleted:    icons_resource.py

--- icons_resource.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'icons_resource.qrc'
-#
-# Created by: PyQt5 UI code generator 5.7
-#
-# WARNING! All changes made in this file will be lost!
-
