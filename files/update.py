fe600989822a46bae4189f2655fb9b2f03d4d1b8
Mostafa Asadi <mostafaasadi73@gmail.com>
2017-02-20 10:54:07 +0330

    update.py check from github now

--- files/update.py
+++ files/update.py
@@ -2,17 +2,29 @@ import urllib
 from urllib import request
 import webbrowser
 import platform
+import newopen
 
 #finding os_type
 os_type = platform.system()
+
+# latest stable version
+# get information dictionary from github
+updatesource = urllib.request.urlopen('https://persepolisdm.github.io/version',data=None)
+serverdict = updatesource.read().decode("utf-8")
+# save information to file
+file = open('serverdictfile','w') # store in tmp
+file.write(str(serverdict))
+file.close()
+# read as dictionary
+dictvalue = newopen.readDict('serverdictfile') #read from tmp
 
 # Foundation for download latest windows installer
 def winupdatedl():
     # find system architect
     if platform.architecture()[0] == '64bit':
-        webbrowser.open('http://137.74.154.122/persepolis/latest_64.exe')
+        webbrowser.open(dictvalue['win64dlurl'])
     elif platform.architecture()[0] == '32bit':
-        webbrowser.open('http://137.74.154.122/persepolis/latest_32.exe')
+        webbrowser.open(dictvalue['win32dlurl'])
     else:
         print("we can't detect your system architect")
 
@@ -20,9 +32,9 @@ def winupdatedl():
 def updatecheck():
     # installed version
     clientversion = '2.3'
-    # latest stable version
-    updatesource = urllib.request.urlopen("http://137.74.154.122/persepolis/version",data=None)
-    serverversion = updatesource.read()
+    # get latest stable version
+    serverversion = dictvalue['version']
+
     # Comparison
     if serverversion != clientversion :
         return False # means client version is dead
