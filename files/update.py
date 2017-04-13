33209e132f5ba2d681b257b58afd1c37fab04d5d
alireza-amirsamimi <alireza.amirsamimi@gmail.com>
2017-04-13 07:59:25 +0430

    urllib library is replaced by requests library name of some variables is changed. winupdatedl is eliminated. and other small changes :) 	modified:   files/update.py

--- files/update.py
+++ files/update.py
@@ -23,102 +23,63 @@ from newopen import Open
 from functools import partial
 import osCommands
 import sys
-import urllib
-from urllib import request
+import requests
 import webbrowser
 import platform
 import newopen
+import ast
 
 # finding os_type
 os_type = platform.system()
 home_address = str(os.path.expanduser("~"))
 
-# persepolis tmp folder (temporary folder)
-if os_type != 'Windows':
-    user_name_split = home_address.split('/')
-    user_name = user_name_split[2]
-    persepolis_tmp = '/tmp/persepolis_' + user_name + '/'
-else:
-    persepolis_tmp = os.path.join(
-        str(home_address), 'AppData', 'Local', 'persepolis_tmp/\/')
+
 
 class checkupdate(QWidget):
     def __init__(self, persepolis_setting):
         super().__init__()
 
+# defining UI
         self.persepolis_setting = persepolis_setting
         icons = ':/' + \
             str(self.persepolis_setting.value('settings/icons')) + '/'
         self.setWindowIcon(QIcon.fromTheme('persepolis', QIcon(':/icon.svg')))
-        self.setWindowTitle('Persepolis Download Manager update')
 
-        # get information dictionary from github
-        updatesource = urllib.request.urlopen('https://persepolisdm.github.io/version', data=None)
-        serverdict = updatesource.read().decode("utf-8")
-        # save information to file
-        fileup = open(persepolis_tmp + 'serverdictfile', 'w')  # store in tmp
-        fileup.write(str(serverdict))
-        fileup.close()
-        # read as dictionary
-        dictvalue = newopen.readDict(persepolis_tmp + 'serverdictfile')  # read from tmp
+        self.setWindowTitle('Checking for newer version')
+
         # installed version
-        clientversion = self.persepolis_setting.value('version/version')
-        # get latest stable version
-        serverversion = dictvalue['version']
-
-        # Foundation for download latest windows installer
-        def winupdatedl():
-            # find system architect
-            if platform.architecture()[0] == '64bit':
-                webbrowser.open(dictvalue['win64dlurl'])
-            elif platform.architecture()[0] == '32bit':
-                webbrowser.open(dictvalue['win32dlurl'])
-
-        # checking function
-        def updatecheck():
-            #Comparison
-            if float(serverversion) > float(clientversion):
-                statustext.setText('A newer Persepolis release is available')
-                # check if it is Windows
-                if os_type == "Windows":
-                    winupdatedl() # this function download latest release
-                if os_type == 'Darwin':
-                    webbrowser.open(dictvalue['macdlurl']) # it download latest release for mac
-            elif float(serverversion) == float(clientversion):
-                statustext.setText('Latest version is installed :)')
-            elif float(serverversion) < float(clientversion):
-                statustext.setText('You are using beta version')
+        self.client_version = self.persepolis_setting.value('version/version')
 
         # first line text
-        uptext = QLabel("The newest is the best , We recommend to update Persepolis")
-        uptext.setTextFormat(QtCore.Qt.RichText)
-        uptext.setAlignment(QtCore.Qt.AlignCenter)
+        self.update_label = QLabel("The newest is the best , We recommend to update Persepolis")
+        self.update_label.setTextFormat(QtCore.Qt.RichText)
+        self.update_label.setAlignment(QtCore.Qt.AlignCenter)
 
         # second line text
-        versiontext = QLabel('This is Persepolis Download Manager version ' + self.persepolis_setting.value('version/version'))
-        versiontext.setAlignment(QtCore.Qt.AlignCenter)
+        self.version_label = QLabel('This is Persepolis Download Manager version ' + self.persepolis_setting.value('version/version'))
+        self.version_label.setAlignment(QtCore.Qt.AlignCenter)
 
         # release link
-        linktext = QLabel('<a href=https://github.com/persepolisdm/persepolis/releases>https://github.com/persepolisdm/persepolis/releases</a>')
-        linktext.setAlignment(QtCore.Qt.AlignCenter)
-        linktext.setOpenExternalLinks(True)
+        self.link_label = QLabel('<a href=https://github.com/persepolisdm/persepolis/releases>https://github.com/persepolisdm/persepolis/releases</a>')
+        self.link_label.setAlignment(QtCore.Qt.AlignCenter)
+        self.link_label.setOpenExternalLinks(True)
 
         # version status
-        statustext = QLabel()
-        statustext.setTextFormat(QtCore.Qt.RichText)
-        statustext.setAlignment(QtCore.Qt.AlignCenter)
+        self.status_label = QLabel()
+        self.status_label.setTextFormat(QtCore.Qt.RichText)
+        self.status_label.setAlignment(QtCore.Qt.AlignCenter)
 
         # update button
-        checkbutton = QPushButton("Check for new update")
-        checkbutton.clicked.connect(updatecheck)
+        self.check_button = QPushButton("Check for new update")
+        self.check_button.clicked.connect(self.updateCheck)
 
         # verticalLayout
         vbox = QVBoxLayout()
-        vbox.addWidget(uptext)
-        vbox.addWidget(versiontext)
-        vbox.addWidget(linktext)
-        vbox.addWidget(checkbutton)
-        vbox.addWidget(statustext)
+        vbox.addWidget(self.update_label)
+        vbox.addWidget(self.version_label)
+        vbox.addWidget(self.link_label)
+        vbox.addWidget(self.check_button)
+        vbox.addWidget(self.status_label)
 
         # horizontalLayout
         hbox = QHBoxLayout()
@@ -135,6 +96,48 @@ class checkupdate(QWidget):
 
         self.resize(size)
         self.move(position)
+
+
+        # checking methode
+    def updateCheck(self, button):
+        self.check_button.setText('Checking...')
+
+        try:
+            # get information dictionary from github
+            updatesource = requests.get('https://persepolisdm.github.io/version')
+
+            updatesource_text = updatesource.text
+            updatesource_dict = ast.literal_eval(updatesource_text)
+
+            # get latest stable version
+            server_version = updatesource_dict['version']
+
+            # Comparison
+            if float(server_version) > float(self.client_version):
+                self.status_label.setText('A newer Persepolis release is available')
+
+                if os_type == 'Windows':
+                    self.winUpdatedl() # this function download latest release
+                    # find system architect
+                    if platform.architecture()[0] == '64bit':
+                        webbrowser.open(dictvalue['win64dlurl'])
+                    elif platform.architecture()[0] == '32bit':
+                        webbrowser.open(dictvalue['win32dlurl'])
+
+                elif os_type == 'Darwin':
+                    webbrowser.open(dictvalue['macdlurl']) # it will download latest release for mac
+
+            elif float(server_version) == float(self.client_version):
+                self.status_label.setText('Latest version is installed :)')
+
+            elif float(server_version) < float(self.client_version):
+                self.status_label.setText('You are using beta version')
+
+        except Exception as e:
+            self.status_label.setText('an error occured while checking update.')
+
+        self.check_button.setText('Check for new update')
+
 
     def closeEvent(self, event):
         # saving window size and position
