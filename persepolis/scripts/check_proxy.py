437c71e39fde3d8a2da4f94b07712db4c1da119f
Mostafa Asadi <mostafaasadi73@gmail.com>
2017-07-13 01:56:02 +0430

    proxy detection

--- persepolis/scripts/check_proxy.py
+++ persepolis/scripts/check_proxy.py
@@ -90,3 +90,149 @@ class Privoxy:
         if self.check_http() or self.check_https():
             return True
         return False
+
+# By Mostafa
+import urllib, requests,os
+
+# TODO: mac os socks proxy, check ftp proxy suport
+
+# get proxy function
+def getproxy():
+    # check if it is KDE
+    if os.environ.get('XDG_CURRENT_DESKTOP') == 'KDE' :
+        # all print just for debugung
+        print(os.environ.get('XDG_CURRENT_DESKTOP'))
+
+        # creat empty list for proxies
+        proxy = {}
+        # user home directory
+        home_address = os.path.expanduser("~")
+
+        # read kde plasma proxy config file
+        try:
+            with open(home_address + '/.config/kioslaverc') as proxyfile:
+                for line in proxyfile:
+                    name, var = line.partition("=")[::2]
+                    proxy[name.strip()] = str(var)
+        except Exception as e:
+            # all print just for debugung
+            print("Error")
+
+        # get ftp proxy
+        try :
+            ftpProxyPort = proxy['ftpProxy'].split(' ')[1]
+            ftpProxyIp = proxy['ftpProxy'].split(' ')[0]
+            # all print just for debugung
+            print('FTP Proxy IP : ' + ftpProxyIp)
+            print('FTP Proxy Port : ' + ftpProxyPort)
+        except Exception as e :
+            ftpProxyIp = False
+            # all print just for debugung
+            print('Error')
+
+        # get http proxy
+        try:
+            httpProxyPort = proxy['httpProxy'].split(' ')[1]
+            httpProxyIp = proxy['httpProxy'].split(' ')[0]
+            # all print just for debugung
+            print('HTTP Proxy IP : ' + httpProxyIp)
+            print('HTTP Proxy Port : ' + httpProxyPort)
+        except Exception as e :
+            httpProxyIp = False
+            # all print just for debugung
+            print('Error')
+
+        # get https proxy
+        try:
+            httpsProxyPort = proxy['httpsProxy'].split(' ')[1]
+            httpsProxyIp = proxy['httpsProxy'].split(' ')[0]
+            # all print just for debugung
+            print('HTTPS Proxy IP : ' + httpsProxyIp)
+            print('HTTPS Proxy Port : ' + httpsProxyPort)
+        except Exception as e:
+            httpsProxyIp = False
+            # all print just for debugung
+            print('Error')
+
+        # get socks proxy
+        try:
+            socksProxyPort = proxy['socksProxy'].split(' ')[1]
+            socksProxyIp = proxy['socksProxy'].split(' ')[0]
+            # all print just for debugung
+            print('Socks Proxy IP : ' + socksProxyIp)
+            print('Socks Proxy Port : ' + socksProxyPort)
+        except Exception as e:
+            socksProxyIp = False
+            # all print just for debugung
+            print('Error')
+
+        # check if just socks proxy exists
+        if not any ([httpProxyIp , ftpProxyIp , httpsProxyIp]) and socksProxyIp :
+            # all print just for debugung
+            print("persepolis doesn't suport socks!")
+        # atleast there is another proxy except socks
+        else:
+            # all print just for debugung
+            print('no problem')
+
+    # if it is windows,mac and other linux desktop
+    else:
+        # get proxies
+        proxy = urllib.request.getproxies()
+
+        # get http proxy
+        try:
+            httpProxyIp = 'http:' + proxy['http'].split(':')[1]
+            httpProxyPort = proxy['http'].split(':')[2]
+            # all print just for debugung
+            print('HTTP Proxy IP : ' + httpProxyIp)
+            print('HTTP Proxy Port : ' + httpProxyPort)
+        except Exception as e :
+            # all print just for debugung
+            print("Error")
+            httpProxyIp = False
+
+        # get https proxy
+        try:
+            httpsProxyIp = 'https:' + proxy['https'].split(':')[1]
+            httpsProxyPort = proxy['https'].split(':')[2]
+            # all print just for debugung
+            print('HTTPS Proxy IP : ' + httpsProxyIp)
+            print('HTTP Proxy Port : ' + httpsProxyPort)
+        except Exception as e :
+            # all print just for debugung
+            print("Error")
+            httpsProxyIp = False
+
+        # get ftp proxy
+        try:
+            ftpProxyIp = 'ftp:' + proxy['ftp'].split(':')[1]
+            ftpProxyPort = proxy['ftp'].split(':')[2]
+            # all print just for debugung
+            print('FTP Proxy IP : ' + ftpProxyIp)
+            print('FTP Proxy Port : ' + ftpProxyPort)
+        except Exception as e :
+            # all print just for debugung
+            print("Error")
+            ftpProxyIp = False
+
+        # get socks proxy
+        try:
+            socksProxyIp = 'socks:' + proxy['socks'].split(':')[1]
+            socksProxyPort = proxy['socks'].split(':')[2]
+            # all print just for debugung
+            print('Socks Proxy IP : ' + socksProxyIp)
+            print('Socks Proxy Port : ' + socksProxyPort)
+        except Exception as e :
+            # all print just for debugung
+            print("Error")
+            socksProxyIp = False
+
+        # check if just socks proxy exists
+        if not any ([httpProxyIp , ftpProxyIp , httpsProxyIp]) and socksProxyIp :
+            # all print just for debugung
+            print("persepolis doesn't suport socks!")
+       # atleast there is another proxy except socks
+        else:
+            # all print just for debugung
+            print('no problem')
