f313a38f2ac95e1be3834a91f40aef91d76eb854
Mostafa Asadi <mostafaasadi73@gmail.com>
2017-07-13 23:50:20 +0430

    socks proxy on GNOME

--- persepolis/scripts/check_proxy.py
+++ persepolis/scripts/check_proxy.py
@@ -1,100 +1,6 @@
-import platform
-import subprocess
-from shutil import which
-
-class Proxy:
-    def check_proxy_manual():
-        """ True If Proxy Settings Is Configures As Manual """
-        proxy = subprocess.check_output(['gsettings', 'get', 'org.gnome.system.proxy', 'mod'])
-        proxy = proxy.decode('utf-8')
-        proxy = proxy.split('\n')[0]
-
-        return (proxy == 'manual')
-
-    def check_proxy_auto():
-        """ True If Proxy Settings Is Configures As Auto """
-        proxy = subprocess.check_output(['gsettings', 'get', 'org.gnome.system.proxy', 'mod'])
-        proxy = proxy.decode('utf-8')
-        proxy = proxy.split('\n')[0]
-
-        return (proxy == 'auto')
-
-    def check_proxy(self):
-        """ Checks the Proxy Status """
-        if self.check_proxy_auto():
-            return ('auto')
-        elif self.check_proxy_manual():
-            return ('manual')
-        return None
-
-class Tor:
-    def __init__(self):
-        if platform.system() == 'linux':
-            self.tor = which('tor')
-        else:
-            self.tor = None
-
-    def check_tor(self):
-        """ True If Tor Is Installed """
-        return (self.tor is not None)
-
-    def socks_tor(self):
-        """ Checks If Socks Proxy Is Configured For Tor """
-        if Proxy().check_proxy_manual() and self.check_tor():
-            socks = subprocess.check_output(['gsettings', 'get', 'org.gnome.system.proxy.socks', 'port'])
-            socks = tor.decode('utf-8')
-            socks = tor.split('\n')[0]
-
-            if tor == '9050':
-                return True
-            return False
-
-class Privoxy:
-    def __init__(self):
-        if platform.system() == 'linux':
-            self.privoxy = which('privoxy')
-        else:
-            self.privoxy = None
-
-    def check_http(self):
-        if Proxy().check_proxy_manual() and self.privoxy is not None:
-            proxy_host = subprocess.check_output(['gsettings', 'get', 'org.gnome.system.proxy.http', 'host'])
-            proxy_host = proxy_host.decode('utf-8')
-            proxy_host = proxy_host.split('\n')[0]
-
-            proxy_port = subprocess.check_output(['gsettings', 'get', 'org.gnome.system.proxy.http', 'port'])
-            proxy_port = proxy_port.decode('utf-8')
-            proxy_port = proxy_port.split('\n')[0]
-
-            if proxy_host == '127.0.0.1' or proxy_host == 'localhost':
-                if proxy_port == '8118':
-                    return True
-            return False
-
-    def check_https(self):
-        if Proxy().check_proxy_manual() and self.privoxy is not None:
-            proxy_host = subprocess.check_output(['gsettings', 'get', 'org.gnome.system.proxy.https', 'host'])
-            proxy_host = proxy_host.decode('utf-8')
-            proxy_host = proxy_host.split('\n')[0]
-
-            proxy_port = subprocess.check_output(['gsettings', 'get', 'org.gnome.system.proxy.https', 'port'])
-            proxy_port = proxy_port.decode('utf-8')
-            proxy_port = proxy_port.split('\n')[0]
-
-            if proxy_host == '127.0.0.1' or proxy_host == 'localhost':
-                if proxy_port == '8118':
-                    return True
-            return False
-
-    def check_privoxy(self):
-        if self.check_http() or self.check_https():
-            return True
-        return False
-
-# By Mostafa
 import urllib, requests,os
 
-# TODO: mac os socks proxy, check ftp proxy suport
+# TODO: mac os socks proxy
 
 # get proxy function
 def getproxy():
@@ -118,62 +24,70 @@ def getproxy():
             # all print just for debugung
             print("Error")
 
-        # get ftp proxy
-        try :
-            ftpProxyPort = proxy['ftpProxy'].split(' ')[1]
-            ftpProxyIp = proxy['ftpProxy'].split(' ')[0]
-            # all print just for debugung
-            print('FTP Proxy IP : ' + ftpProxyIp)
-            print('FTP Proxy Port : ' + ftpProxyPort)
-        except Exception as e :
-            ftpProxyIp = False
-            # all print just for debugung
-            print('Error')
+        # check proxy enabled as manually
+        if proxy['ProxyType'].split('\n')[0] == '1' :
+            # get ftp proxy
+            try :
+                ftpProxyPort = proxy['ftpProxy'].split(' ')[1].replace("/", "").replace("\n", "")
+                ftpProxyIp = proxy['ftpProxy'].split(' ')[0]
+                # all print just for debugung
+                print('FTP Proxy IP : ' + ftpProxyIp)
+                print('FTP Proxy Port : ' + ftpProxyPort)
+            except Exception as e :
+                ftpProxyIp = False
+                # all print just for debugung
+                print('Error')
 
-        # get http proxy
-        try:
-            httpProxyPort = proxy['httpProxy'].split(' ')[1]
-            httpProxyIp = proxy['httpProxy'].split(' ')[0]
-            # all print just for debugung
-            print('HTTP Proxy IP : ' + httpProxyIp)
-            print('HTTP Proxy Port : ' + httpProxyPort)
-        except Exception as e :
-            httpProxyIp = False
-            # all print just for debugung
-            print('Error')
+            # get http proxy
+            try:
+                httpProxyPort = proxy['httpProxy'].split(' ')[1].replace("/", "").replace("\n", "")
+                httpProxyIp = proxy['httpProxy'].split(' ')[0]
+                # all print just for debugung
+                print('HTTP Proxy IP : ' + httpProxyIp)
+                print('HTTP Proxy Port : ' + httpProxyPort)
+            except Exception as e :
+                httpProxyIp = False
+                # all print just for debugung
+                print('Error')
 
-        # get https proxy
-        try:
-            httpsProxyPort = proxy['httpsProxy'].split(' ')[1]
-            httpsProxyIp = proxy['httpsProxy'].split(' ')[0]
-            # all print just for debugung
-            print('HTTPS Proxy IP : ' + httpsProxyIp)
-            print('HTTPS Proxy Port : ' + httpsProxyPort)
-        except Exception as e:
-            httpsProxyIp = False
-            # all print just for debugung
-            print('Error')
+            # get https proxy
+            try:
+                httpsProxyPort = proxy['httpsProxy'].split(' ')[1].replace("/", "").replace("\n", "")
+                httpsProxyIp = proxy['httpsProxy'].split(' ')[0]
+                # all print just for debugung
+                print('HTTPS Proxy IP : ' + httpsProxyIp)
+                print('HTTPS Proxy Port : ' + httpsProxyPort)
+            except Exception as e:
+                httpsProxyIp = False
+                # all print just for debugung
+                print('Error')
 
-        # get socks proxy
-        try:
-            socksProxyPort = proxy['socksProxy'].split(' ')[1]
-            socksProxyIp = proxy['socksProxy'].split(' ')[0]
-            # all print just for debugung
-            print('Socks Proxy IP : ' + socksProxyIp)
-            print('Socks Proxy Port : ' + socksProxyPort)
-        except Exception as e:
-            socksProxyIp = False
-            # all print just for debugung
-            print('Error')
+            # get socks proxy
+            try:
+                socksProxyPort = proxy['socksProxy'].split(' ')[1].replace("/", "").replace("\n", "")
+                socksProxyIp = proxy['socksProxy'].split(' ')[0]
+                # all print just for debugung
+                print('Socks Proxy IP : ' + socksProxyIp)
+                print('Socks Proxy Port : ' + socksProxyPort)
+            except Exception as e:
+                socksProxyIp = False
+                # all print just for debugung
+                print('Error')
 
-        # check if just socks proxy exists
-        if not any ([httpProxyIp , ftpProxyIp , httpsProxyIp]) and socksProxyIp :
-            # all print just for debugung
-            print("persepolis doesn't suport socks!")
-        # atleast there is another proxy except socks
+            # check if just socks proxy exists
+            if not any ([httpProxyIp , ftpProxyIp , httpsProxyIp]) and socksProxyIp :
+                # all print just for debugung
+                print("persepolis doesn't suport socks!")
+            # atleast there is another proxy except socks
+            else:
+                # all print just for debugung
+                print('no problem')
+
+        # proxy disabled
         else:
             # all print just for debugung
-            print('no problem')
+            print('proxy disabled')
+
 
     # if it is windows,mac and other linux desktop
     else:
@@ -183,7 +97,7 @@ def getproxy():
         # get http proxy
         try:
             httpProxyIp = 'http:' + proxy['http'].split(':')[1]
-            httpProxyPort = proxy['http'].split(':')[2]
+            httpProxyPort = proxy['http'].split(':')[2].replace("/", "").replace("\n", "")
             # all print just for debugung
             print('HTTP Proxy IP : ' + httpProxyIp)
             print('HTTP Proxy Port : ' + httpProxyPort)
@@ -195,7 +109,7 @@ def getproxy():
         # get https proxy
         try:
             httpsProxyIp = 'https:' + proxy['https'].split(':')[1]
-            httpsProxyPort = proxy['https'].split(':')[2]
+            httpsProxyPort = proxy['https'].split(':')[2].replace("/", "").replace("\n", "")
             # all print just for debugung
             print('HTTPS Proxy IP : ' + httpsProxyIp)
             print('HTTP Proxy Port : ' + httpsProxyPort)
@@ -207,7 +121,7 @@ def getproxy():
         # get ftp proxy
         try:
             ftpProxyIp = 'ftp:' + proxy['ftp'].split(':')[1]
-            ftpProxyPort = proxy['ftp'].split(':')[2]
+            ftpProxyPort = proxy['ftp'].split(':')[2].replace("/", "").replace("\n", "")
             # all print just for debugung
             print('FTP Proxy IP : ' + ftpProxyIp)
             print('FTP Proxy Port : ' + ftpProxyPort)
@@ -218,11 +132,20 @@ def getproxy():
 
         # get socks proxy
         try:
-            socksProxyIp = 'socks:' + proxy['socks'].split(':')[1]
-            socksProxyPort = proxy['socks'].split(':')[2]
-            # all print just for debugung
-            print('Socks Proxy IP : ' + socksProxyIp)
-            print('Socks Proxy Port : ' + socksProxyPort)
+            # if it is gnome
+            if os.environ.get('XDG_CURRENT_DESKTOP') == 'GNOME' :
+                socksProxyIp = 'socks:' + proxy['all'].split(':')[1]
+                socksProxyPort = proxy['all'].split(':')[2].replace("/", "").replace("\n", "")
+                # all print just for debugung
+                print('Socks Proxy IP : ' + socksProxyIp)
+                print('Socks Proxy Port : ' + socksProxyPort)
+            # other desktop
+            else:
+                socksProxyIp = 'socks:' + proxy['socks'].split(':')[1]
+                socksProxyPort = proxy['socks'].split(':')[2].replace("/", "").replace("\n", "")
+                # all print just for debugung
+                print('Socks Proxy IP : ' + socksProxyIp)
+                print('Socks Proxy Port : ' + socksProxyPort)
         except Exception as e :
             # all print just for debugung
             print("Error")
@@ -236,3 +159,7 @@ def getproxy():
         else:
             # all print just for debugung
             print('no problem')
+
+
+if __name__ == '__main__':
+    getproxy()
