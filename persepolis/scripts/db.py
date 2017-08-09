e3b27085d03bb493bdd82721930d233f86dc43d3
alireza-amirsamimi <alireza.amirsamimi@gmail.com>
2017-08-09 14:28:08 +0430

    persepolis db is created! addlink table is created 	modified:   persepolis/scripts/db.py

--- persepolis/scripts/db.py
+++ persepolis/scripts/db.py
@@ -15,3 +15,53 @@
 """
 
 import sqlite3
+
+
+
+# download manager data_base path .
+if os_type == 'Linux' or os_type == 'FreeBSD' or os_type == 'OpenBSD':
+    data_base_path = os.path.join(
+        str(home_address), ".config/persepolis_download_manager/persepolis.db")
+elif os_type == 'Darwin':
+    data_base_path = os.path.join(
+        str(home_address), "Library/Application Support/persepolis_download_manager/persepolis.db")
+elif os_type == 'Windows':
+    data_base_path = os.path.join(
+        str(home_address), 'AppData', 'Local', 'persepolis_download_manager', 'persepolis.db')
+
+
+sqlite_connection = sqlite3.connect(data_base_path)
+sqlite_cursor = sqlite_connection.cursor()
+
+
+# contains addlink window download information
+add_link_table = sqlite_cursor.execute("""CREATE TABLE IF NOT EXISTS WITHOUT ROWID addlink(
+                                                                                            gid NOT NULL TEXT PRIMARY KEY,
+                                                                                            last_try_date TEXT,
+                                                                                            firs_try_date TEXT,
+                                                                                            out TEXT,
+                                                                                            final_download_path TEXT,
+                                                                                            start_hour TEXT,
+                                                                                            start_minute TEXT,
+                                                                                            end_hour TEXT,
+                                                                                            end_minute TEXT,
+                                                                                            link TEXT,
+                                                                                            ip TEXT,
+                                                                                            port TEXT,
+                                                                                            proxy_user TEXT,
+                                                                                            proxy_passwd TEXT,
+                                                                                            download_user TEXT,
+                                                                                            download_passwd TEXT,
+                                                                                            connections TEXT,
+                                                                                            limit TEXT,
+                                                                                            download_path TEXT
+                                                                                            )""") 
+
+# contains download table download items information
+
+# contains gid of downloads and download situation (active or not active) and category of download
+
+# contains name of categories
+
+
+
