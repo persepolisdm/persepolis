1f0c891b0efebf4207a10dd9af2f274b3c730bb8
Mostafa Asadi <mostafaasadi73@gmail.com>
2017-11-14 01:40:10 +0330

    proxy final fix

--- persepolis/scripts/check_proxy.py
+++ persepolis/scripts/check_proxy.py
@@ -1,5 +1,4 @@
 import urllib
-import requests
 import os
 from persepolis.scripts import logger
 import platform
@@ -10,6 +9,7 @@ os_type = platform.system()
 platform = 'platform : ' + os_type
 logger.sendToLog(platform, "INFO")
 
+
 # get proxy function
 def getProxy():
     socks_proxy = False
@@ -18,7 +18,7 @@ def getProxy():
     desktop = os.environ.get('XDG_CURRENT_DESKTOP')
     proxy = {}
     if os_type == 'Linux' or os_type == 'FreeBSD' or os_type == 'OpenBSD':
-        if desktop == None:
+        if desktop is None:
             desktop_env_type = 'Desktop Environment not detected!'
         else:
             desktop_env_type = 'Desktop environment: ' + str(desktop)
@@ -27,57 +27,70 @@ def getProxy():
         print(desktop_env_type)
 
     # check if it is KDE
-    if desktop == 'KDE' :
+    if desktop == 'KDE':
         # creat empty list for proxies
         proxysource = {}
 
         # user home directory path
         home_address = os.path.expanduser("~")
-
-        # read kde plasma proxy config file
+        # get proxy file content
         try:
-            plasma_proxy_config_file_path = os.path.join(home_address, '.config', 'kioslaverc')
-            with open(plasma_proxy_config_file_path) as proxyfile:
-                for line in proxyfile:
-                    name, var = line.partition("=")[::2]
-                    proxysource[name.strip()] = str(var)
+            plasma_proxy_config_file_path = os.path.join(
+                home_address,
+                '.config',
+                'kioslaverc'
+            )
         except:
             logger.sendToLog('no proxy file detected', 'INFO')
 
-        # check proxy enabled as manually
-        if proxysource['ProxyType'].split('\n')[0] == '1' :
-            # get ftp proxy
+        # check if proxy file exists
+        if os.path.isfile(plasma_proxy_config_file_path):
+            # read kde plasma proxy config file
             try:
-                proxy['ftp_proxy_port'] = proxysource['ftpProxy'].split(' ')[1].replace("/", "").replace("\n", "")
-                proxy['ftp_proxy_ip'] = proxysource['ftpProxy'].split(' ')[0].split('//')[1]
+                with open(plasma_proxy_config_file_path) as proxyfile:
+                    for line in proxyfile:
+                        name, var = line.partition("=")[::2]
+                        proxysource[name.strip()] = str(var)
             except:
-                logger.sendToLog('no manuall ftp proxy detected', 'INFO')
+                logger.sendToLog('no proxy file detected', 'INFO')
 
-            # get http proxy
-            try:
-                proxy['http_proxy_port'] = proxysource['httpProxy'].split(' ')[1].replace("/", "").replace("\n", "")
-                proxy['http_proxy_ip'] = proxysource['httpProxy'].split(' ')[0].split('//')[1]
-            except:
-                logger.sendToLog('no manuall http proxy detected', 'INFO')
+            # check proxy enabled as manually
+            if proxysource['ProxyType'].split('\n')[0] == '1':
+                # get ftp proxy
+                try:
+                    proxy['ftp_proxy_port'] = proxysource['ftpProxy'].split(' ')[1].replace("/", "").replace("\n", "")
+                    proxy['ftp_proxy_ip'] = proxysource['ftpProxy'].split(' ')[0].split('//')[1]
+                except:
+                    logger.sendToLog('no manuall ftp proxy detected', 'INFO')
 
-            # get https proxy
-            try:
-                proxy['https_proxy_port'] = proxysource['httpsProxy'].split(' ')[1].replace("/", "").replace("\n", "")
-                proxy['https_proxy_ip'] = proxysource['httpsProxy'].split(' ')[0].split('//')[1]
-            except:
-                logger.sendToLog('no manuall https proxy detected', 'INFO')
+                # get http proxy
+                try:
+                    proxy['http_proxy_port'] = proxysource['httpProxy'].split(' ')[1].replace("/", "").replace("\n", "")
+                    proxy['http_proxy_ip'] = proxysource['httpProxy'].split(' ')[0].split('//')[1]
+                except:
+                    logger.sendToLog('no manuall http proxy detected', 'INFO')
 
-            # get socks proxy
-            try:
-                socks_proxy = proxysource['socksProxy'].split(' ')[0].split('//')[1]
+                # get https proxy
+                try:
+                    proxy['https_proxy_port'] = proxysource['httpsProxy'].split(' ')[1].replace("/", "").replace("\n", "")
+                    proxy['https_proxy_ip'] = proxysource['httpsProxy'].split(' ')[0].split('//')[1]
+                except:
+                    logger.sendToLog('no manuall https proxy detected', 'INFO')
 
-            except:
-                socks_proxy = False
+                # get socks proxy
+                try:
+                    socks_proxy = proxysource['socksProxy'].split(' ')[0].split('//')[1]
 
-        # proxy disabled
+                except:
+                    socks_proxy = False
+
+            # proxy disabled
+            else:
+                logger.sendToLog('no manuall proxy detected', 'INFO')
+
+        # proxy file not exists
         else:
-            logger.sendToLog('no manuall proxy detected', 'INFO')
-
+            logger.sendToLog('no proxy file detected', 'INFO')
 
     # if it is windows,mac and other linux desktop
     else:
@@ -107,10 +120,10 @@ def getProxy():
         # get socks proxy
         try:
             # if it is gnome or unity
-            if desktop == 'GNOME' or desktop == 'Unity:Unity7' :
-                socks_proxy = proxysource['all'].split(':')[1].replace('//','')
+            if desktop == 'GNOME' or desktop == 'Unity:Unity7':
+                socks_proxy = proxysource['all'].split(':')[1].replace('//', '')
             # if it is Mac OS
-            elif os_type == 'Darwin' :
+            elif os_type == 'Darwin':
 
                 validKeys = ['SOCKSEnable']
 
@@ -122,13 +135,13 @@ def getProxy():
                     if len(words) == 3 and words[0] in validKeys:
                         mac_tmp_proxies_list[words[0]] = words[2]
 
-                if mac_tmp_proxies_list['SOCKSEnable'] is '1' :
+                if mac_tmp_proxies_list['SOCKSEnable'] is '1':
                     socks_proxy = True
                 else:
                     socks_proxy = False
             # others except KDE,Mac OS,gnome,unity7
             else:
-                socks_proxy = proxysource['socks'].split(':')[1].replace('//','')
+                socks_proxy = proxysource['socks'].split(':')[1].replace('//', '')
         except:
             socks_proxy = False
 
@@ -139,8 +152,7 @@ def getProxy():
         if key in proxy.keys():
             key_is_available = True
 
-
-    if not key_is_available and socks_proxy :
+    if not key_is_available and socks_proxy:
         # all print just for debugung
         socks_message = "persepolis and aria2 don't support socks\n\
         you must convert socks proxy to http proxy.\n\
