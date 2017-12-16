0e40a8a53e539a5106d76be7c8a7a2145b5782aa
alireza-amirsamimi <alireza.amirsamimi@gmail.com>
2017-12-16 22:34:48 +0330

    Issue #348 is fixed! progress_ui BUG is fixed. 	modified:   persepolis/gui/progress_ui.py

--- persepolis/gui/progress_ui.py
+++ persepolis/gui/progress_ui.py
@@ -16,13 +16,9 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QWidget, QSizePolicy
+from PyQt5.QtWidgets import QCheckBox, QProgressBar, QFrame, QDoubleSpinBox, QComboBox, QWidget, QTabWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QWidget, QSizePolicy
 from PyQt5.QtGui import QIcon
-import ast
-import os
 from persepolis.scripts.newopen import Open
-
-home_address = os.path.expanduser("~")
 
 
 class ProgressWindow_Ui(QWidget):
@@ -37,123 +33,141 @@ class ProgressWindow_Ui(QWidget):
         self.setWindowIcon(QIcon.fromTheme('persepolis', QIcon(':/persepolis.svg')))
         self.setWindowTitle("Persepolis Download Manager")
 
-        self.gridLayout = QtWidgets.QGridLayout(self)
-        self.verticalLayout = QtWidgets.QVBoxLayout()
+        verticalLayout = QVBoxLayout(self)
+
 # progress_tabWidget
-        self.progress_tabWidget = QtWidgets.QTabWidget(self)
-# informations_tab
-        self.informations_tab = QWidget()
-        self.informations_verticalLayout = QtWidgets.QVBoxLayout(
-            self.informations_tab)
+        self.progress_tabWidget = QTabWidget(self)
+
+# information_tab
+        self.information_tab = QWidget()
+        information_verticalLayout = QVBoxLayout(self.information_tab)
+
 # link_label
-        self.link_label = QtWidgets.QLabel(self.informations_tab)
-        self.informations_verticalLayout.addWidget(self.link_label)
+        self.link_label = QLabel(self.information_tab)
+        information_verticalLayout.addWidget(self.link_label)
 
 # status_label
-        self.status_label = QtWidgets.QLabel(self.informations_tab)
-        self.informations_verticalLayout.addWidget(self.status_label)
+        self.status_label = QLabel(self.information_tab)
+        information_verticalLayout.addWidget(self.status_label)
+
 # downloaded_label
-        self.downloaded_label = QtWidgets.QLabel(self.informations_tab)
-        self.informations_verticalLayout.addWidget(self.downloaded_label)
+        self.downloaded_label = QLabel(self.information_tab)
+        information_verticalLayout.addWidget(self.downloaded_label)
+
 # rate_label
-        self.rate_label = QtWidgets.QLabel(self.informations_tab)
-        self.informations_verticalLayout.addWidget(self.rate_label)
+        self.rate_label = QLabel(self.information_tab)
+        information_verticalLayout.addWidget(self.rate_label)
+
 # time_label
-        self.time_label = QtWidgets.QLabel(self.informations_tab)
-        self.informations_verticalLayout.addWidget(self.time_label)
+        self.time_label = QLabel(self.information_tab)
+        information_verticalLayout.addWidget(self.time_label)
 
-        self.connections_label = QtWidgets.QLabel(self.informations_tab)
-        self.informations_verticalLayout.addWidget(self.connections_label)
+# connections_label
+        self.connections_label = QLabel(self.information_tab)
+        information_verticalLayout.addWidget(self.connections_label)
 
-        self.progress_tabWidget.addTab(self.informations_tab, "")
+# add information_tab to progress_tabWidget
+        self.progress_tabWidget.addTab(self.information_tab, "")
+
 # options_tab
-        self.options_tab = QtWidgets.QWidget()
-        self.widget = QtWidgets.QWidget(self.options_tab)
-        self.widget.setGeometry(QtCore.QRect(30, 7, 511, 151))
-
-        self.options_gridLayout = QtWidgets.QGridLayout(self.widget)
-        self.options_gridLayout.setContentsMargins(0, 0, 0, 0)
+        self.options_tab = QWidget()
+        options_tab_horizontalLayout = QHBoxLayout(self.options_tab)
+        
 # limit_checkBox
-        self.limit_checkBox = QtWidgets.QCheckBox(self.widget)
-        self.options_gridLayout.addWidget(self.limit_checkBox, 0, 0, 1, 1)
-# after_checkBox
-        self.after_checkBox = QtWidgets.QCheckBox(self.widget)
-        self.options_gridLayout.addWidget(self.after_checkBox, 0, 1, 1, 1)
+        self.limit_checkBox = QCheckBox(self.options_tab)
+
+        limit_verticalLayout = QVBoxLayout()
+        limit_verticalLayout.addWidget(self.limit_checkBox)
+
 # limit_frame
-        self.limit_frame = QtWidgets.QFrame(self.widget)
+        self.limit_frame = QFrame(self.options_tab)
         self.limit_frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.limit_frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        limit_frame_verticalLayout = QVBoxLayout(self.limit_frame)
+        limit_frame_horizontalLayout = QHBoxLayout()
 
-        self.widget1 = QtWidgets.QWidget(self.limit_frame)
-        self.widget1.setGeometry(QtCore.QRect(44, 27, 151, 62))
-        self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.widget1)
-        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
 # limit_spinBox
-        self.limit_spinBox = QtWidgets.QDoubleSpinBox(self.widget1)
+        self.limit_spinBox = QDoubleSpinBox(self.options_tab)
         self.limit_spinBox.setMinimum(1)
         self.limit_spinBox.setMaximum(1023)
-        self.horizontalLayout.addWidget(self.limit_spinBox)
+        limit_frame_horizontalLayout.addWidget(self.limit_spinBox)
+
 # limit_comboBox
-        self.limit_comboBox = QtWidgets.QComboBox(self.widget1)
+        self.limit_comboBox = QComboBox(self.options_tab)
         self.limit_comboBox.addItem("")
         self.limit_comboBox.addItem("")
-        self.horizontalLayout.addWidget(self.limit_comboBox)
-        self.verticalLayout_2.addLayout(self.horizontalLayout)
+
+        limit_frame_horizontalLayout.addWidget(self.limit_comboBox)
+
 # limit_pushButton
-        self.limit_pushButton = QtWidgets.QPushButton(self.widget1)
-        self.verticalLayout_2.addWidget(self.limit_pushButton)
-        self.options_gridLayout.addWidget(self.limit_frame, 1, 0, 1, 1)
+        self.limit_pushButton = QPushButton(self.options_tab)
+        
+        limit_frame_verticalLayout.addLayout(limit_frame_horizontalLayout)
+        limit_frame_verticalLayout.addWidget(self.limit_pushButton)
+
+        limit_verticalLayout.addWidget(self.limit_frame)
+
+        options_tab_horizontalLayout.addLayout(limit_verticalLayout)
+
+# after_checkBox
+        self.after_checkBox = QCheckBox(self.options_tab)
+
+        after_verticalLayout = QVBoxLayout()
+        after_verticalLayout.addWidget(self.after_checkBox)
+
 # after_frame
-        self.after_frame = QtWidgets.QFrame(self.widget)
+        self.after_frame = QFrame(self.options_tab)
         self.after_frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.after_frame.setFrameShadow(QtWidgets.QFrame.Raised)
+
+        after_frame_verticalLayout = QVBoxLayout(self.after_frame)
+
 # after_comboBox
-        self.widget = QtWidgets.QWidget(self.after_frame)
-        self.widget.setGeometry(QtCore.QRect(77, 28, 150, 60))
-
-        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.widget)
-        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
-
-        self.after_comboBox = QtWidgets.QComboBox(self.after_frame)
-        self.after_comboBox.setGeometry(QtCore.QRect(73, 46, 111, 26))
+        self.after_comboBox = QComboBox(self.options_tab)
         self.after_comboBox.addItem("")
 
-        self.verticalLayout_3.addWidget(self.after_comboBox)
-# after_pushButton
-        self.after_pushButton = QtWidgets.QPushButton(self.widget)
-        self.verticalLayout_3.addWidget(self.after_pushButton)
+        after_frame_verticalLayout.addWidget(self.after_comboBox)
 
-        self.options_gridLayout.addWidget(self.after_frame, 1, 1, 1, 1)
+# after_pushButton
+        self.after_pushButton = QPushButton(self.options_tab)
+        after_frame_verticalLayout.addWidget(self.after_pushButton)
+
+        after_verticalLayout.addWidget(self.after_frame)
+
+        options_tab_horizontalLayout.addLayout(after_verticalLayout)
+
         self.progress_tabWidget.addTab(self.options_tab, "")
-        self.verticalLayout.addWidget(self.progress_tabWidget)
+
+        verticalLayout.addWidget(self.progress_tabWidget)
 
 
 # download_progressBar
-        self.download_progressBar = QtWidgets.QProgressBar(self)
-        self.verticalLayout.addWidget(self.download_progressBar)
+        self.download_progressBar = QProgressBar(self)
+        verticalLayout.addWidget(self.download_progressBar)
 
-        self.button_horizontalLayout = QtWidgets.QHBoxLayout()
-        spacerItem = QtWidgets.QSpacerItem(
-            40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.button_horizontalLayout.addItem(spacerItem)
+# buttons
+        button_horizontalLayout = QHBoxLayout()
+        button_horizontalLayout.addStretch(1)
+
 # resume_pushButton
-        self.resume_pushButton = QtWidgets.QPushButton(self)
-        self.button_horizontalLayout.addWidget(self.resume_pushButton)
+        self.resume_pushButton = QPushButton(self)
         self.resume_pushButton.setIcon(QIcon(icons + 'play'))
+        button_horizontalLayout.addWidget(self.resume_pushButton)
+
 # pause_pushButton
         self.pause_pushButton = QtWidgets.QPushButton(self)
-        self.button_horizontalLayout.addWidget(self.pause_pushButton)
         self.pause_pushButton.setIcon(QIcon(icons + 'pause'))
+        button_horizontalLayout.addWidget(self.pause_pushButton)
+
 # stop_pushButton
         self.stop_pushButton = QtWidgets.QPushButton(self)
-        self.button_horizontalLayout.addWidget(self.stop_pushButton)
-        self.verticalLayout.addLayout(self.button_horizontalLayout)
-        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
         self.stop_pushButton.setIcon(QIcon(icons + 'stop'))
+        button_horizontalLayout.addWidget(self.stop_pushButton)
+
+
+        verticalLayout.addLayout(button_horizontalLayout)
 
         self.progress_tabWidget.setCurrentIndex(0)
-        QtCore.QMetaObject.connectSlotsByName(self)
 # labels
         self.link_label.setText("Link :")
         self.status_label.setText("Status : ")
@@ -162,7 +176,7 @@ class ProgressWindow_Ui(QWidget):
         self.time_label.setText("Estimate time left :")
         self.connections_label.setText("Number of connections : ")
         self.progress_tabWidget.setTabText(self.progress_tabWidget.indexOf(
-            self.informations_tab),  "Download informations")
+            self.information_tab),  "Download information")
         self.limit_checkBox.setText("Limit Speed")
         self.after_checkBox.setText("After download")
         self.limit_comboBox.setItemText(0,  "KB/S")
