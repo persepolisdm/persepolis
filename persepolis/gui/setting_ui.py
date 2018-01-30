4764cec40af74b5b0e99acea362772f3cbaccf52
N/A <hayyan71@yahoo.com>
2018-01-30 15:41:43 +0330

    Update setting_ui.py

--- persepolis/gui/setting_ui.py
+++ persepolis/gui/setting_ui.py
@@ -29,10 +29,10 @@ class Setting_Ui(QWidget):
 
 # add support for other languages
 # a) detect current value of locale in persepolis config file
-        if str(self.persepolis_setting.value('settings/locale')) in (-1, 'en_US'):
+        if str(persepolis_setting.value('settings/locale')) in (-1, 'en_US'):
             locale_dir = ''
         else:
-            locale_dir = 'locales/' + str(self.persepolis_setting.value('settings/locale')) + '/ui.qm'
+            locale_dir = 'locales/' + str(persepolis_setting.value('settings/locale')) + '/ui.qm'
         locale_path = pkg_resources.resource_filename(__name__, locale_dir)
 # b) set translator to Qtranslator
         self.translator = QTranslator()
