eb8f5b506654c44a1017eb3ae2866d6316f95632
alireza-amirsamimi <alireza.amirsamimi@gmail.com>
2018-02-05 16:24:30 +0330

    Paths are edited. 	modified:   resources/translation_gen.py

--- resources/translation_gen.py
+++ resources/translation_gen.py
@@ -9,8 +9,8 @@ HERE = os.path.dirname(os.path.realpath(__file__))
 translation_files = os.path.join(HERE, 'translation_files.pro')
 
 def function(locale):
-    if not os.path.exists(os.path.join(HERE + '/icons/locales')):
-        os.makedirs(os.path.join(HERE + '/icons/locales'))
+    if not os.path.exists(os.path.join(HERE, 'locales')):
+        os.makedirs(os.path.join(HERE, 'locales'))
 
     subprocess.call(['pylupdate5', '-noobsolete','-tr-function', 'ui_tr', translation_files])
 function("locale")
