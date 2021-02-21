96045a8fadcdefd19a02a5a12ec615cb96b9c9d9
uidops <uidopsu@gmail.com>
2021-02-21 11:57:16 +0330

    clean code

--- persepolis/scripts/__init__.py
+++ persepolis/scripts/__init__.py
@@ -12,8 +12,3 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
-
-# import sys
-
-# sys.path.insert(1, '.')
