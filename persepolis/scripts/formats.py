ac2042d117556af49e85da39b12bb24d42ac450b
uidops <uidopsu@gmail.com>
2021-02-21 11:23:54 +0330

    add formats.py

---
+++ persepolis/scripts/formats.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class Formats:
+    AUDIO = ['aa', 'aac', 'aax', 'act', 'aiff', 'amr', 'ape', 'au', 'awb', 'cda', 'dct', 'dss', 'dvf', 'flac', 'gsm', 'iklax',
+            'ivs', 'm4a', 'm4p', 'mmf', 'mp3', 'mpc', 'msv', 'oga', 'ogg', 'opus', 'ra', 'raw', 'sln', 'tta', 'vox', 'wav', 'wma', 'wv']
+    
+    VIDEO = ['3g2', '3gp', 'asf', 'avi', 'drc', 'f4a', 'f4b', 'f4p', 'f4v', 'flv', 'gif', 'm2v', 'm4p',
+            'm4v', 'mkv', 'mng', 'mov', 'mp2', 'mp4', 'mpe', 'mpeg', 'mpg', 'mpv', 'mxf', 'nsv', 'ogv',
+            'qt', 'rm', 'rmvb', 'roq', 'svi', 'viv', 'vob', 'webm', 'wmv', 'yuv']
+
+    
+    DOCUMENT = ['azw', 'azw3', 'azw4', 'cb7', 'cba', 'cbr', 'cbt', 'cbz', 'chm', 'djvu', 'doc', 'docx', 'epub', 'fb2',
+                'htm', 'html', 'ibooks', 'kf8', 'md', 'odt', 'pdf', 'ps', 'pubmobi', 'rtf', 'sxw', 'tex', 'txt']
+    
+    COMPRESSED =  ['7z', 'F', 'LBR', 'Z', 'a', 'ace', 'afa', 'alz', 'apk', 'ar', 'arc', 'arj', 'b1', 'ba', 'bh', 'bz2', 'cab', 'cfs', 'cpio', 'cpt', 'dar', 'dd',
+                'dgc', 'dmg', 'ear', 'ecc', 'gca', 'gz', 'ha', 'hki', 'ice', 'infl', 'iso', 'jar', 'kgb', 'lbr', 'lha', 'lz', 'lzh', 'lzma', 'lzo', 'lzx', 'mar', 'pac', 'paq6', 'paq7', 'paq8', 'par', 'par2',
+                'partimg', 'pea', 'pim', 'pit', 'qda', 'rar', 'rk', 'rz', 's7z', 'sda', 'sea', 'sen', 'sfark', 'sfx', 'shar', 'sit', 'sitx', 'sqx', 'sz', 'tar', 'tar.Z', 'tar.bz2', 'tar.gz', 'tar.lzma', 'tbz2',
+                'tgz', 'tlz', 'uc', 'uc0', 'uc2', 'uca', 'ucn', 'ue2', 'uha', 'ur2', 'war', 'wim', 'xar', 'xp3', 'xz', 'yz1', 'z', 'zip', 'zipx', 'zoo', 'zpaq', 'zz']
+
+    
