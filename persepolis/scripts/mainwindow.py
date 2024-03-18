b95acb30139c027e73544ec110c6245628aa20e9
Mostafa Asadi <mostafaasadi73@gmail.com>
2024-03-18 16:57:51 +0330

    fix: change ffmpeg path in standalone runtime

--- persepolis/scripts/mainwindow.py
+++ persepolis/scripts/mainwindow.py
@@ -149,9 +149,8 @@ class CheckVersionsThread(QThread):
         if os_type in OS.UNIX_LIKE:
 
             # find ffmpeg_path for standalone version of Persepolis
-            cwd = sys.argv[0]
-            current_directory = os.path.dirname(cwd)
-            ffmpeg_path_standaloe = os.path.join(current_directory, 'ffmpeg')
+            base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+            ffmpeg_path_standaloe = os.path.join(base_path, 'ffmpeg')
 
             if os.path.exists(ffmpeg_path_standaloe):
 
